d5e960383c09faccd6adc94a6f04c02262ed4421
vaibhavsinghtaurus <vaibhav.singh@thinktaurus.com>
2026-04-24 16:16:16 +0530

    [Feature] - allowed netlify origin

--- src/teaorcoffee/main.py
+++ src/teaorcoffee/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1|192\.168\.1\.\d+)(:\d+)?|https://.*\.onrender\.com",
+    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1|192\.168\.1\.\d+)(:\d+)?|https://.*\.netlify\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
